182be0582feb6f6817279762796222ce4f125c66
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-07-12 23:45:42 +0530

    added terser-plugin to minify javascript

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const merge = require("webpack-merge");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "production",
@@ -12,7 +13,7 @@ module.exports = merge(common, {
         path: path.resolve(__dirname, "dist"),
     },
     optimization: {
-        minimizer: [new OptimizeCssAssetsPlugin()],
+        minimizer: [new OptimizeCssAssetsPlugin(), new TerserPlugin()],
     },
     plugins: [
         new MiniCssExtractPlugin({ filename: "[name].[contentHash].scss" }),
