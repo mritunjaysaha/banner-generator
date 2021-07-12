004bff3a26cd57edbdeca0e3c17108dddbf77299
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-07-12 21:33:43 +0530

    added webpack

---
+++ webpack.common.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
+    module: {
+        rules: [
+            {
+                test: /\.scss/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+        ],
+    },
+};
