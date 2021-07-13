e78845826b68e6906b4af0328dc7638ec764138e
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-07-13 13:10:31 +0530

    webpack working

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const common = require("./webpack.common");
 const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "development",
